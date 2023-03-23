6b2f5593fe507ab1547c1eac36a932d7fc1e7e3f
Nick Johnson <nick@yonson.dev>
2023-03-23 16:16:55 -0700

    Better min neighbor distance default

--- cmd/raiju/raiju.go
+++ cmd/raiju/raiju.go
@@ -45,7 +45,7 @@ func main() {
 	minCapacity := candidatesFlagSet.Int64("min-capacity", 1000000, "Minimum capacity of a node in satoshis")
 	minChannels := candidatesFlagSet.Int64("min-channels", 1, "Candidate must have at least this many channels")
 	minDistance := candidatesFlagSet.Int64("min-distance", 2, "Candidate must be at least this far away (0 is root node and 1 is direct connection)")
-	minNeighborDistance := candidatesFlagSet.Int64("min-neighbor-distance", 2, "Minimum distance from root node to be considered a valuable neighbor of candidate")
+	minNeighborDistance := candidatesFlagSet.Int64("min-neighbor-distance", 3, "Minimum distance from root node to be considered a valuable neighbor of candidate")
 	pubkey := candidatesFlagSet.String("pubkey", "", "Node to span out from, defaults to the connected node")
 	assume := candidatesFlagSet.String("assume", "", "Comma separated pubkeys to assume channels too")
 	limit := candidatesFlagSet.Int64("limit", 100, "Number of results")
@@ -69,8 +69,8 @@ func main() {
 				return errors.New("min-distance must be greater than 1")
 			}
 
-			if *minNeighborDistance < 2 {
-				return errors.New("min-neighbor-distance must be greater than 1")
+			if *minNeighborDistance < 3 {
+				return errors.New("min-neighbor-distance must be greater than 2")
 			}
 
 			cfg := &lndclient.LndServicesConfig{
